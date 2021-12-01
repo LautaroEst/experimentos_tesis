3b438d90cdd5d94cbe8952ddba8d8e4680dfb709
LautaroEst <lautaro.est@gmail.com>
2021-11-30 21:10:51 -0300

    orden en la sala

--- experimentos/01_classification_models/all_models/run_test_melisa.sh
+++ experimentos/01_classification_models/all_models/run_test_melisa.sh
@@ -1,23 +1,35 @@
 #! /bin/bash
 
-declare -a modelsarray=("gtp2-esp")
+declare -a modelsarray=("beto-uncased" "electricidad" "mbert-sent")
 
 for m in "${modelsarray[@]}"
 do
     echo ""
     echo "Melisa-2. Model: $m"
     echo ""
-    python main.py --model $m --dataset "melisa-2" --test --dropout 0.0 --num_epochs 1 --batch_size 2 --learning_rate 1e-5 --device "cuda:1" --eval_every 100
+    python main.py --model $m --dataset "melisa-2" --test --dropout 0.05 --num_epochs 1 --batch_size 8 --learning_rate 1e-5 --device "cuda:1" --eval_every 100
     echo ""
     echo ""
     echo "Melisa-3. Model: $m"
     echo ""
-    python main.py --model $m --dataset "melisa-3" --test --dropout 0.0 --num_epochs 1 --batch_size 2 --learning_rate 1e-5 --device "cuda:1" --eval_every 100
+    python main.py --model $m --dataset "melisa-3" --test --dropout 0.05 --num_epochs 1 --batch_size 8 --learning_rate 1e-5 --device "cuda:1" --eval_every 100
     echo ""
     echo "Melisa-5. Model: $m"
     echo ""
-    python main.py --model $m --dataset "melisa-5" --test --dropout 0.0 --num_epochs 1 --batch_size 2 --learning_rate 1e-5 --device "cuda:1" --eval_every 100
+    python main.py --model $m --dataset "melisa-5" --test --dropout 0.05 --num_epochs 1 --batch_size 8 --learning_rate 1e-5 --device "cuda:1" --eval_every 100
 done
 
-
+echo ""
+echo "Melisa-2. Model: gtp2-esp"
+echo ""
+python main.py --model "gtp2-esp" --dataset "melisa-2" --test --dropout 0.05 --num_epochs 1 --batch_size 2 --learning_rate 1e-5 --device "cuda:1" --eval_every 100
+echo ""
+echo ""
+echo "Melisa-3. Model: gtp2-esp"
+echo ""
+python main.py --model "gtp2-esp" --dataset "melisa-3" --test --dropout 0.05 --num_epochs 1 --batch_size 2 --learning_rate 1e-5 --device "cuda:1" --eval_every 100
+echo ""
+echo "Melisa-5. Model: gtp2-esp"
+echo ""
+python main.py --model "gtp2-esp" --dataset "melisa-5" --test --dropout 0.05 --num_epochs 1 --batch_size 2 --learning_rate 1e-5 --device "cuda:1" --eval_every 100
 
